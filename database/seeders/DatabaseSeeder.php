4e2d5dd7b30e7ff695677b8303ce35ac606747c2
Matheus Ramos <matheusr808@gmail.com>
2024-03-04 22:41:48 -0300

    crimes infinitos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -43,64 +43,215 @@ class DatabaseSeeder extends Seeder
             ['name' => 'Tráfico de pessoas'],
         ]);
 
+        //IPIOCA
         Report::create([
             'crime_id' => '5',
             'osm_type' => 'N',
             'osm_id' => '1234567890',
-            'lat' => '-9.664',
-            'lon' => '-35.701',
+            'lat' => '-9.524677', 
+            'lon' => '-35.606986',
+            'date' => '2024-02-27',
+            'time' => '23:45:01'
+        ]);
+        Report::create([
+            'crime_id' => '7',
+            'osm_type' => 'N',
+            'osm_id' => '1234567890',
+            'lat' => '-9.526024', 
+            'lon' => '-35.609512',
+            'date' => '2024-02-27',
+            'time' => '23:45:01'
+        ]);
+        //PESCARIA
+        Report::create([
+            'crime_id' => '8',
+            'osm_type' => 'N',
+            'osm_id' => '1234567890',
+            'lat' => '-9.538612', 
+            'lon' => '-35.621694',
+            'date' => '2024-02-27',
+            'time' => '23:45:01'
+        ]);
+        Report::create([
+            'crime_id' => '13',
+            'osm_type' => 'N',
+            'osm_id' => '1234567890',
+            'lat' => '-9.537610', 
+            'lon' => '-35.619965',
+            'date' => '2024-02-27',
+            'time' => '23:45:01'
+        ]);
+        //RIACHO DOCE
+        Report::create([
+            'crime_id' => '2',
+            'osm_type' => 'N',
+            'osm_id' => '1234567890',
+            'lat' => '-9.576342', 
+            'lon' => '-35.660680',
+            'date' => '2024-02-27',
+            'time' => '23:45:01'
+        ]);
+        Report::create([
+            'crime_id' => '9',
+            'osm_type' => 'N',
+            'osm_id' => '1234567890',
+            'lat' => '-9.574091', 
+            'lon' => '-35.658172',
+            'date' => '2024-02-27',
+            'time' => '23:45:01'
+        ]);
+        //GARÇA TORTA
+        Report::create([
+            'crime_id' => '17',
+            'osm_type' => 'N',
+            'osm_id' => '1234567890',
+            'lat' => '-9.582046', 
+            'lon' => '-35.666848',
+            'date' => '2024-02-27',
+            'time' => '23:45:01'
+        ]);
+        Report::create([
+            'crime_id' => '4',
+            'osm_type' => 'N',
+            'osm_id' => '1234567890',
+            'lat' => '-9.579379', 
+            'lon' => '-35.663215',
+            'date' => '2024-02-27',
+            'time' => '23:45:01'
+        ]);
+        //GUAXUMA
+        Report::create([
+            'crime_id' => '6',
+            'osm_type' => 'N',
+            'osm_id' => '1234567890',
+            'lat' => '-9.595075', 
+            'lon' => '-35.678017',
+            'date' => '2024-02-27',
+            'time' => '23:45:01'
+        ]);
+        Report::create([
+            'crime_id' => '11',
+            'osm_type' => 'N',
+            'osm_id' => '1234567890',
+            'lat' => '-9.593832',  
+            'lon' => '-35.681753',
+            'date' => '2024-02-27',
+            'time' => '23:45:01'
+        ]);
+        //JACARECICA
+        Report::create([
+            'crime_id' => '3',
+            'osm_type' => 'N',
+            'osm_id' => '1234567890',
+            'lat' => '-9.582539', 
+            'lon' => '-35.705192',
+            'date' => '2024-02-27',
+            'time' => '23:45:01'
+        ]);
+        Report::create([
+            'crime_id' => '15',
+            'osm_type' => 'N',
+            'osm_id' => '1234567890',
+            'lat' => '-9.580547',  
+            'lon' => '-35.701259',
+            'date' => '2024-02-27',
+            'time' => '23:45:01'
+        ]);
+        //CRUZ DAS ALMAS
+        Report::create([
+            'crime_id' => '10',
+            'osm_type' => 'N',
+            'osm_id' => '1234567890',
+            'lat' => '-9.627310',
+            'lon' => '-35.707687',
+            'date' => '2024-02-27',
+            'time' => '23:45:01'
+        ]);
+        Report::create([
+            'crime_id' => '17',
+            'osm_type' => 'N',
+            'osm_id' => '1234567890',
+            'lat' => '-9.625374', 
+            'lon' => '-35.707848',
+            'date' => '2024-02-27',
+            'time' => '23:45:01'
+        ]);
+        //BENEDITO BENTES
+        Report::create([
+            'crime_id' => '17',
+            'osm_type' => 'N',
+            'osm_id' => '1234567890',
+            'lat' => '-9.539858',
+            'lon' => '-35.721529',
+            'date' => '2024-02-27',
+            'time' => '23:45:01'
+        ]);
+        Report::create([
+            'crime_id' => '1',
+            'osm_type' => 'N',
+            'osm_id' => '1234567890',
+            'lat' => '-9.567206', 
+            'lon' => '-35.718163',
+            'date' => '2024-02-27',
+            'time' => '23:45:01'
+        ]);
+        //ANTARES
+        Report::create([
+            'crime_id' => '5',
+            'osm_type' => 'N',
+            'osm_id' => '1234567890',
+            'lat' => '-9.583499', 
+            'lon' => '-35.731653',
+            'date' => '2024-02-27',
+            'time' => '23:45:01'
+        ]);
+        Report::create([
+            'crime_id' => '10',
+            'osm_type' => 'N',
+            'osm_id' => '1234567890',
+            'lat' => '-9.567230', 
+            'lon' => '-35.746485',
+            'date' => '2024-02-27',
+            'time' => '23:45:01'
+        ]);
+        //CIDADE UNIVERSITARIA
+        Report::create([
+            'crime_id' => '1',
+            'osm_type' => 'N',
+            'osm_id' => '1234567890',
+            'lat' => '-9.553442', 
+            'lon' => '-35.776745',
+            'date' => '2024-02-27',
+            'time' => '23:45:01'
+        ]);
+        Report::create([
+            'crime_id' => '8',
+            'osm_type' => 'N',
+            'osm_id' => '1234567890',
+            'lat' => '-9.531442', 
+            'lon' => '-35.774363',
+            'date' => '2024-02-27',
+            'time' => '23:45:01'
+        ]);
+        //POÇO
+        Report::create([
+            'crime_id' => '3',
+            'osm_type' => 'N',
+            'osm_id' => '1234567890',
+            'lat' => '-9.662112', 
+            'lon' => '-35.723071',
+            'date' => '2024-02-27',
+            'time' => '23:45:01'
+        ]);
+        Report::create([
+            'crime_id' => '5',
+            'osm_type' => 'N',
+            'osm_id' => '1234567890',
+            'lat' => '-9.656882', 
+            'lon' => '-35.715096',
             'date' => '2024-02-27',
             'time' => '23:45:01'
         ]);
 
-        Report::create([
-            'crime_id' => '6',
-            'osm_type' => 'W',
-            'osm_id' => '1234567891',
-            'lat' => '-9.664',
-            'lon' => '-35.702',
-            'date' => '2024-02-27',
-            'time' => '23:45:02'
-        ]);
-
-        Report::create([
-            'crime_id' => '7',
-            'osm_type' => 'R',
-            'osm_id' => '1234567892',
-            'lat' => '-9.664',
-            'lon' => '-35.703',
-            'date' => '2024-02-27',
-            'time' => '23:45:03'
-        ]);
-
-        Report::create([
-            'crime_id' => '8',
-            'osm_type' => 'N',
-            'osm_id' => '1234567893',
-            'lat' => '-9.664',
-            'lon' => '-35.704',
-            'date' => '2024-02-27',
-            'time' => '23:45:04'
-        ]);
-
-        Report::create([
-            'crime_id' => '9',
-            'osm_type' => 'W',
-            'osm_id' => '1234567894',
-            'lat' => '-9.664',
-            'lon' => '-35.705',
-            'date' => '2024-02-27',
-            'time' => '23:45:05'
-        ]);
-
-        Report::create([
-            'crime_id' => '10',
-            'osm_type' => 'R',
-            'osm_id' => '1234567895',
-            'lat' => '-9.664',
-            'lon' => '-35.706',
-            'date' => '2024-02-27',
-            'time' => '23:45:06'
-        ]);
     }
 }
