0301c1f216ee13069ba8cdedc2d0af295a37d099
unknown <gcco@ic.ufal.br>
2024-03-19 19:05:55 -0300

    Feat: Send reports to backend

--- resources/js/components/ReportModal.js
+++ resources/js/components/ReportModal.js
@@ -17,16 +17,50 @@ fetchCrimes().then(data => {
     const crimeType = document.getElementById("crimeType");
     data.forEach(crime => {
         var option = document.createElement("option");
-        option.value = crime.name;
+        option.value = crime.id;
         option.text = crime.name;
         crimeType.appendChild(option);
     });
 }
 ).catch(err => {console.log(err);});
 
-try {
-    var currentLocationModal = JSON.parse(localStorage.getItem('currentLocationModal'));
-    console.log(currentLocationModal.name);
-} catch (error) {
-    var currentLocationModal = "";
-}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var reportForm = document.getElementById('report_form');
+
+    reportForm.addEventListener('submit', function(event) {
+        //event.preventDefault();
+        try {
+            var currentLocationModal = JSON.parse(localStorage.getItem('currentLocationModal'));
+            console.log(currentLocationModal.name);
+            var formData = {
+                //TEST
+                crime_id: document.getElementById('crimeType').value,
+                //END
+                osm_type: currentLocationModal.osm_type[0].toUpperCase(),
+                osm_id: currentLocationModal.osm_id,
+                lat: currentLocationModal.lat,
+                lon: currentLocationModal.lon,
+                date: document.getElementById('date_ocorrencia').value,
+                time: document.getElementById('time_ocorrencia').value,
+            };
+            console.log(formData);
+            fetch(apiURL + 'reports/store', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData)
+            })
+            .then(response => response.json())
+            .then(data => {
+                // Lidar com a resposta aqui
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    });
+});
