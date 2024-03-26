5408e66a6c7483ca7edf4ef1d009e09cf8ec78e9
Lael Santa Rosa <lael_lsr@hotmail.com>
2024-03-26 11:46:49 -0300

    update: welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,8 +20,8 @@
         <img src="{{ Vite::asset('resources/img/logo_1.png') }}" class="home-logo" alt="Safety Map - Percorra com segurança">
         <div class="mt-4">
             <h3>Verifique o nível de criminalidade do seu local de destino</h3>
-            <p class="mt-3 lh-base">Pretende visitar ou morar em um local desconhecido?<br>Use o nosso mapa para ter acesso aos índices de violência em qualquer lugar do mundo!</p>
-            <a class="btn btn-dark btn-lg px-3 text-uppercase fw-bolder" href="map">Acessar mapa</a>
+            <p class="mt-3 mb-5 lh-base">Pretende visitar ou morar em um local desconhecido? Use o nosso mapa<br>para ter acesso aos índices de violência em qualquer lugar do mundo!</p>
+            <a class="btn btn-outline-light px-4 text-uppercase fw-bolder" href="map">Acessar mapa</a>
         </div>
     </div>
     {{-- <div class="d-flex justify-content-center mt-5">
